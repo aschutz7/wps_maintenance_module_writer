f7742da8ae5718fd77e61a1c3065ad90f0b7c958
Azazel-Official <anthonyschutzz@gmail.com>
2025-01-16 20:57:22 -0600

    Final commit to add Sentry logging

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,31 +1,29 @@
-const {
-    sentryWebpackPlugin
-} = require("@sentry/webpack-plugin");
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin');
 
 module.exports = {
-    /**
+	/**
 	 * This is the main entry point for your application, it's the first file
 	 * that runs in the main process.
 	 */
-    entry: './src/main.js',
+	entry: './src/main.js',
 
-    // Put your normal webpack config below here
-    module: {
+	// Put your normal webpack config below here
+	module: {
 		rules: require('./webpack.rules'),
 	},
 
-    externals: {
+	externals: {
 		xlsx: 'commonjs xlsx',
 		docx: 'commonjs docx',
 		mammoth: 'commonjs mammoth',
 		'puppeteer-core': 'commonjs puppeteer-core',
 	},
-
-    devtool: "source-map",
-
-    plugins: [sentryWebpackPlugin({
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-        org: "wps-engineering",
-        project: "electron"
-    })]
+	devtool: 'source-map',
+	plugins: [
+		sentryWebpackPlugin({
+			authToken: process.env.SENTRY_AUTH_TOKEN,
+			org: 'wps-engineering',
+			project: 'electron',
+		}),
+	],
 };
